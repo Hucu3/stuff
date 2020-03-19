29e18ae1e9945170b4410281c596558c5d3eded5
Angel Ernesto Medrano Santibañez <ernesto.medrano@hotmail.com>
2020-03-18 19:37:32 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-#Aquí va el código
-#Esta línea solo es para crear una copia por cualquier cosa del archivo a modificar
-########cp /etc/network/interfaces /etc/network/interfaces.bak
-# Esta sección presenta las opciones necesarias
-echo "Para ajustar la red usando DHCP escribe: 1"
-echo "Para ajustar la red con ip estático en casa escribe: 2"
-echo "Para ajustar la red con ip estático el trabajo escribe: 3"
-echo ""
-echo ""
-#Esta sección es para esperar la elección del usuario
+
 read -p "Enter value of i :" i
 
 if [ $i -eq 10 ]
